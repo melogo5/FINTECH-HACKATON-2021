805edc1f41b469bed083d7b15ce3397a9c7ee1d8
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2021-12-12 17:30:31 +0500

    большие первые буквы

--- src/client/pages/page-profile.js
+++ src/client/pages/page-profile.js
@@ -56,11 +56,11 @@ export default class PageProfile extends Component {
         <div class="nameProfile">Стив Джобс</div>
         <div class="statusProfile">Неравнодушный</div>
         <div class="donationsProfile">
-          <p>пожертвованно:</p>
+          <p>Пожертвованно:</p>
           <p>1630</p>
         </div>
         <div class="stickersProfile">
-          <p>стикеров:</p>
+          <p>Стикеров:</p>
           <p>5</p>
         </div>
       </template>`;
